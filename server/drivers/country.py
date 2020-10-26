d7deec923052dea5d3eeb3c7c8e160c5af184f0c
David Williams <dwilliams@slashdave.com>
2020-10-26 10:03:20 -0700

    Switch world data sources to "Our World in Data"

--- server/drivers/country.py
+++ server/drivers/country.py
@@ -3,7 +3,7 @@ import pandas as pd
 from datetime import date
 from os import path
 from flask import current_app as app
-import redis, requests, time, pyarrow
+import redis, requests, time, pyarrow, hashlib
 
 
 def connect():
@@ -15,50 +15,80 @@ def fetchGlobal(rconn):
     #
     # Returned cached value, if no more than 10 minutes old
     #
-    expires = rconn.hget("country","expires")
+    expires = rconn.hget("country2","expires")
     if expires and time.time() < float(expires):
-        return context.deserialize(rconn.hget("country","dataframe"))
+        return context.deserialize(rconn.hget("country2","dataframe"))
 
     #
-    # Convert from json
+    # Fetch
     #
-    country_key = requests.get("https://corona-api.com/countries").json()
-
-    answer = pd.DataFrame({
-        'name': [a['name'] for a in country_key['data']],
-        'code': [a['code'] for a in country_key['data']],
-        'population': [a['population'] for a in country_key['data']]
-    })
+    answer = pd.read_csv("https://covid.ourworldindata.org/data/ecdc/locations.csv")
+    answer['code'] = answer['countriesAndTerritories']
+    answer = answer.rename(columns={'location':'name'})
 
     #
     # Cache
     #
-    rconn.hset("country","dataframe",context.serialize(answer).to_buffer().to_pybytes())
-    rconn.hset("country","expires",str(time.time()+600.0))
+    rconn.hset("country2","dataframe",context.serialize(answer).to_buffer().to_pybytes())
+    rconn.hset("country2","expires",str(time.time()+600.0))
     return answer
 
 
-def fetchCountry(rconn,code="US"):
+#
+# For ourworldindata, tables are returned for all countries,
+# since this method now fetches all countries, and simply
+# extracts the relative information from the appropriate tables. 
+#
+def fetchCases(rconn):
     context = pyarrow.default_serialization_context()
 
-    expires = rconn.hget("country",code+"_expires")
+    #
+    # Returned cached value, if no more than 10 minutes old
+    #
+    expires = rconn.hget("country2cases","expires")
     if expires and time.time() < float(expires):
-        return context.deserialize(rconn.hget("country",code))
+        return context.deserialize(rconn.hget("country2cases","dataframe"))
 
-    usdata = requests.get("http://corona-api.com/countries/"+code).json()['data']
+    answer = pd.read_csv("https://covid.ourworldindata.org/data/ecdc/new_cases.csv",parse_dates=["date"])
 
-    answer = pd.DataFrame({
-        'dt': pd.to_datetime([date(int(a['date'][0:4]),int(a['date'][5:7]),int(a['date'][8:10])) for a in usdata['timeline']]),
-        'cases': [a['new_confirmed'] for a in usdata['timeline']],
-        'deaths': [a['new_deaths'] for a in usdata['timeline']],
-        'recovered': [a['new_recovered'] for a in usdata['timeline']],
-        'prog': ['is_in_progress' in a for a in usdata['timeline']]
-    })
-    if len(answer): answer = answer[~answer.prog].sort_values(by="dt")
-
-    rconn.hset("country",code,context.serialize(answer).to_buffer().to_pybytes())
-    rconn.hset("country",code+"_expires",str(time.time()+600.0))
+    #
+    # Cache
+    #
+    rconn.hset("country2cases","dataframe",context.serialize(answer).to_buffer().to_pybytes())
+    rconn.hset("country2cases","expires",str(time.time()+600.0))
     return answer
+
+def fetchDeaths(rconn):
+    context = pyarrow.default_serialization_context()
+
+    #
+    # Returned cached value, if no more than 10 minutes old
+    #
+    expires = rconn.hget("country2deaths","expires")
+    if expires and time.time() < float(expires):
+        return context.deserialize(rconn.hget("country2deaths","dataframe"))
+
+    answer = pd.read_csv("https://covid.ourworldindata.org/data/ecdc/new_deaths.csv",parse_dates=["date"])
+
+    #
+    # Cache
+    #
+    rconn.hset("country2deaths","dataframe",context.serialize(answer).to_buffer().to_pybytes())
+    rconn.hset("country2deaths","expires",str(time.time()+600.0))
+    return answer
+
+    
+def fetchCountry(rconn,name="United States"):
+    cases = fetchCases(rconn)
+    deaths = fetchDeaths(rconn)
+
+    cases['cases'] = cases[name].fillna(0).astype(int)
+    deaths['deaths'] = deaths[name].fillna(0).astype(int)
+
+    cases = cases.filter(items=("date","cases"))
+    deaths = deaths.filter(items=("date","deaths"))
+
+    return cases.merge(deaths,on="date").sort_values(by="date")
 
 
 def codes():
@@ -67,8 +97,9 @@ def codes():
 
     return {
         'abbrev': dict(zip(ckey.code,ckey.name)),
-        'default': "US"
+        'default': "United_States_of_America"
     }
+
 
 def menu():
     r = connect()
@@ -76,16 +107,18 @@ def menu():
     ckey.sort_values(by="name")
     return {
         'abbrev': dict(zip(ckey.code,ckey.name)),
-        'default': "US"
+        'default': "United_States_of_America"
     }
+
 
 def plot(code):
     r = connect()
-    dt = fetchCountry(r,code)
     ckey = fetchGlobal(r)
+    dt = fetchCountry(r,ckey[ckey.code==code].name)
+
     dt['croll'] = dt.cases.rolling(window=7).mean()
     dt['droll'] = dt.deaths.rolling(window=7).mean()
-    dt = dt[dt.dt >= pd.to_datetime(date(2020,3,1))]
+    dt = dt[dt.date >= pd.to_datetime(date(2020,3,1))]
 
     #
     # This are fake, so we can make a legend
@@ -98,12 +131,12 @@ def plot(code):
     fake_scale = alt.Scale(domain=('Daily','7 day'), range=('lightgrey','blue'))
 
     case_points = chart.mark_line(point=True).encode(
-        x = alt.X("dt:T",title="Date"),
+        x = alt.X("date:T",title="Date"),
         y = alt.Y("cases:Q",title="Cases"),
         color = alt.Color("src1", scale=fake_scale)
     )
     case_average = chart.mark_line().encode(
-        x = alt.X('dt:T'),
+        x = alt.X('date:T'),
         y = alt.Y('croll:Q'),
         color = alt.Color("src2", scale=fake_scale)
     )
@@ -112,11 +145,11 @@ def plot(code):
         point={"color": "lightgrey"}, 
         color="lightgrey"
     ).encode(
-        x = alt.X("dt:T", title="Date"),
+        x = alt.X("date:T", title="Date"),
         y = alt.Y("deaths:Q",title="Fatalities")
     )
     death_average = chart.mark_line().encode(
-        x = alt.X('dt:T'),
+        x = alt.X('date:T'),
         y = alt.Y('droll:Q')
     )
 
@@ -130,25 +163,25 @@ def plot(code):
     return (top & bot).configure_legend(title=None).to_dict()
 
 
-def countries(codes):
+def countries(names):
     r = connect()
     ckey = fetchGlobal(r)
 
-    def make_one(code):
-        dt = fetchCountry(r,code)
-        ckey0 = ckey[ckey.code == code]
+    def make_one(name):
+        dt = fetchCountry(r,name)
+        ckey0 = ckey[ckey.name == name]
         dt['croll'] = dt.cases.rolling(window=7).mean()*100000/ckey0.population.mean()
         dt['droll'] = dt.deaths.rolling(window=7).mean()*100000/ckey0.population.mean()
-        dt = dt[dt.dt >= pd.to_datetime(date(2020,3,1))]
-        dt['Country'] = ckey0.name.iloc[0]
+        dt = dt[dt.date >= pd.to_datetime(date(2020,3,1))]
+        dt['Country'] = name
         return dt
 
-    dt = pd.concat([make_one(c) for c in codes])
+    dt = pd.concat([make_one(c) for c in names])
 
     chart = alt.Chart(dt)
     
     top = chart.mark_line().encode(
-        x = alt.X('dt:T', title="Date"),
+        x = alt.X('date:T', title="Date"),
         y = alt.Y('croll:Q', title="Cases per 100,000"),
         color = alt.Color("Country:N")
     ).properties(
@@ -157,7 +190,7 @@ def countries(codes):
     )
 
     bot = chart.mark_line().encode(
-        x = alt.X('dt:T', title="Date"),
+        x = alt.X('date:T', title="Date"),
         y = alt.Y('droll:Q', title="Fatalities per 100,000"),
         color = alt.Color("Country:N")
     ).properties(
@@ -169,10 +202,10 @@ def countries(codes):
 
 
 def scandinavia():
-    return countries(['SE','NO','DK'])
+    return countries(['Sweden','Norway','Denmark'])
 
 def north_america():
-    return countries(['US','CA','MX'])
+    return countries(['United States','Canada','Mexico'])
 
 def europe():
-    return countries(['GB','FR','ES','DE'])
+    return countries(['United Kingdom','France','Spain','Germany'])
